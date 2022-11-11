cd1f025caae0a945ab40ca4998a0cbf921329fd7
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-11-11 17:01:07 +0100

    mise à jour du css et debut du js

---
+++ index.js
@@ -0,0 +1,3 @@
+let etudiant = ['Alexandre','Stéphane','Arnaud','Sandrine','Laure','Delphine','Johann','Pierre','Jean-Christophe','Jessie','Jean-François','Terry','Sébastien','Sukunthy','Kévin','Jean-Sébastien']
+
+console.log(etudiant.length);
