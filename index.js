c271accf9a5427f2f5550752bc90c4f182475866
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-11-12 07:30:18 +0100

    test js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let etudiant = ['Alexandre','Stéphane','Arnaud','Sandrine','Laure','Delphine','Johann','Pierre','Jean-Christophe','Jessie','Jean-François','Terry','Sébastien','Sukunthy','Kévin','Jean-Sébastien'];
-let etudiant1 = etudiant[Math.floor(Math.random() * etudiant.length)];
 
 
-setTimeout(()=>{
+function student1(){
+    let etudiant1 = etudiant[Math.floor(Math.random() * etudiant.length)];
     console.log(etudiant1);
-},3000)
+}
+
+function student() { 
+    let etudiant1 = etudiant[Math.floor(Math.random() * etudiant.length)];
+    document.getElementById("remplace").addEventListener("click",student1);
+    document.getElementById("remplace").innerHTML = etudiant1;
+    console.log();
+}
+setInterval(() => {
+    stop.student
+    return;
+}, 3000);    
